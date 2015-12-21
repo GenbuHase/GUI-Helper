f76ab2021421f1bfb36440a130094e07601f223b
Genbu Hase <hasegenbu@gmail.com>
2015-12-21 14:00:46 +0900

    とりあえずファイル読み込みの枠組みを作成。

--- Alpha/Update Checker.js
+++ Alpha/Update Checker.js
@@ -1,3 +1,22 @@
 function UpdateCheck() {
-	var Connecter = new java.net.URLConnection(new java.net.URL("https://github.com/GenbuHase/GUI-Helper/tree/master/Beta/"));
+	new java.lang.Thread(
+		new java.lang.Runnable({
+			run: function () {
+				try {
+					var Connecter = new java.net.URL("https://github.com/GenbuHase/GUI-Helper/tree/master/Alpha/").openConnection().connect();
+					var ModContent = new java.io.BufferedReader(
+						new java.io.InputStreamReader(Connecter.getInputStream())
+					);
+
+					while (ModContent.readLine() != null) {
+						if (ModContent.readLine() == "") {
+							
+						}
+					}
+				} catch (Error) {
+					clientMessage(Error);
+				}
+			}
+		})
+	);
 }
