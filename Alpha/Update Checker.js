330ea4b0f7c153e1a43e7aa0320d169eac354928
Genbu Hase <hasegenbu@gmail.com>
2015-12-22 12:46:29 +0900

    バビロン様のご協力もあり、読み取り処理が完了しました。

--- Alpha/Update Checker.js
+++ Alpha/Update Checker.js
@@ -3,23 +3,52 @@ function UpdateCheck() {
 		new java.lang.Runnable({
 			run: function () {
 				try {
-					var Connecter = new java.net.URL("https://raw.githubusercontent.com/GenbuHase/GUI-Helper/master/Alpha/After.js").openConnection();
-						Connecter.connect();
-
-					var AfterMod = new java.io.BufferedReader(new java.io.InputStreamReader(Connecter.getInputStream()));
-					var AfterContent = "";
-
-					while (AfterMod.readLine() != null) {
-						AfterContent += AfterMod.readLine() + "\n";
+					var Flag = {
+						Before: false,
+						After: false
 					}
 
-					clientMessage(AfterContent);
+					var Connecter = {
+						Before: null,
+						After: new java.net.URL("https://raw.githubusercontent.com/GenbuHase/GUI-Helper/master/Alpha/After.js").openConnection()
+					}
+
+					var Reader = {
+						Before: null,
+						After: new java.io.BufferedReader(new java.io.InputStreamReader(Connecter.After.getInputStream()))
+					}
+
+					var Content = {
+						Before: "",
+						After: "",
+						
+						MemoryBefore: "",
+						MemoryAfter: ""
+					}
+
+					Connecter.After.connect();
+
+					while (!Flag.After) {
+						Content.MemoryAfter = Reader.After.readLine();
+
+						if (Content.MemoryAfter != null) {
+							Content.After += Content.MemoryAfter + "\n";
+						} else {
+							Flag.After = true;
+						}
+					}
+
+					clientMessage(Content.After.toString());
 				} catch (Error) {
 					clientMessage(Error);
 				}
 			}
 		})
 	).start();
+}
+
+function Update() {
+	
 }
 
 function newLevel() {
